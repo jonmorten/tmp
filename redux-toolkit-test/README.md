a0d338084463beff2e8c10ff8cd8f31fe7009764
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2022-09-20 22:06:18 +0200

    tmp

---
+++ redux-toolkit-test/README.md
@@ -0,0 +1,9 @@
+# Redux toolkit test
+
+See:
+
+- https://redux.js.org/tutorials/typescript-quick-start
+- https://github.com/reduxjs/redux/tree/master/examples/counter-ts
+- https://codesandbox.io/s/github/reduxjs/redux/tree/master/examples/counter-ts
+- https://redux.js.org/tutorials/fundamentals/part-8-modern-redux
+- https://redux.js.org/tutorials/essentials/part-8-rtk-query-advanced
---
+++ redux-toolkit-test/src/App.tsx
@@ -0,0 +1,41 @@
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { incremented, amountAdded } from "./features/counter/counter.slice";
+
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const count = useAppSelector((state) => state.counter.value);
+
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    // dispatch(incremented());
+    dispatch(amountAdded(3));
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
